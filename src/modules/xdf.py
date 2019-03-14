3684b196b411505e37e29ad9c5e7dd46406f7162
Keith Cronin <39650357+kthewhispers@users.noreply.github.com>
2019-03-14 00:26:24 -0400

    Polished xdf.py

--- src/modules/xdf.py
+++ src/modules/xdf.py
@@ -1,8 +1,6 @@
 '''
 MIT License
-
 Copyright (c) 2019 Keith Christopher Cronin
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -24,107 +22,107 @@ _hasreadxdf = False
 debug = False
 
 def readxdf(data):
-	global _hasreadxdf
-	isonproperty = False
-	isonpropertyvalue = False
-	isonequal = False
-	isonmarker = True
-	position = 0
-	propertyvaluebuffer = []
-	propertybuffer = []
-	global _parseddictionary
-	propertystartindex = 0
-	propertyendindex = 0
-	propertyvaluestartindex = 0
-	propertyvalueendindex = 0
-	propwasmarked = False
-	propvaluewasmarked = False
-	propertyname = ''
+        global _hasreadxdf
+        isonproperty = False
+        isonpropertyvalue = False
+        isonequal = False
+        isonmarker = True
+        position = 0
+        propertyvaluebuffer = []
+        propertybuffer = []
+        global _parseddictionary
+        propertystartindex = 0
+        propertyendindex = 0
+        propertyvaluestartindex = 0
+        propertyvalueendindex = 0
+        propwasmarked = False
+        propvaluewasmarked = False
+        propertyname = ''
 
-	if data[0] != '@' or _hasreadxdf:
-		return False
-	else:
-		for character in data:
-			if isonproperty:
-				if propwasmarked == False:
-					propertystartindex = position
-					propwasmarked = True
-				else:
-					pass
+        if data[0] != '@' or _hasreadxdf:
+                return False
+        else:
+                for character in data:
+                        if isonproperty:
+                                if propwasmarked == False:
+                                        propertystartindex = position
+                                        propwasmarked = True
+                                else:
+                                        pass
 
-			if character == '=':
-					isonpropertyvalue = True
-					propertyendindex = position
-					propertyname = data[propertystartindex:propertyendindex].strip('\n')
-					propertyvaluestartindex = position + 1
+                        if character == '=':
+                                        isonpropertyvalue = True
+                                        propertyendindex = position
+                                        propertyname = data[propertystartindex:propertyendindex].strip('\n')
+                                        propertyvaluestartindex = position + 1
 
-			if isonpropertyvalue:
-				if character == '@' or position == len(data)-1:
-					isonmarker = True
-					propertyvalueendindex = position
-					_parseddictionary[propertyname] = data[propertyvaluestartindex:propertyvalueendindex].strip('\n')
-					propwasmarked = False
-				else:
-					pass
+                        if isonpropertyvalue:
+                                if character == '@' or position == len(data)-1:
+                                        isonmarker = True
+                                        propertyvalueendindex = position
+                                        _parseddictionary[propertyname] = data[propertyvaluestartindex:propertyvalueendindex].strip('\n')
+                                        propwasmarked = False
+                                else:
+                                        pass
 
-			if isonmarker:
-				isonproperty = True
-				isonmarker = False
-			position += 1
-		_hasreadxdf = True
-		return True
+                        if isonmarker:
+                                isonproperty = True
+                                isonmarker = False
+                        position += 1
+                _hasreadxdf = True
+                return True
 
 def readxdf(data, markercharacter):
-	global _hasreadxdf
-	isonproperty = False
-	isonpropertyvalue = False
-	isonequal = False
-	isonmarker = True
-	position = 0
-	propertyvaluebuffer = []
-	propertybuffer = []
-	global _parseddictionary
-	propertystartindex = 0
-	propertyendindex = 0
-	propertyvaluestartindex = 0
-	propertyvalueendindex = 0
-	propwasmarked = False
-	propvaluewasmarked = False
-	propertyname = ''
+        global _hasreadxdf
+        isonproperty = False
+        isonpropertyvalue = False
+        isonequal = False
+        isonmarker = True
+        position = 0
+        propertyvaluebuffer = []
+        propertybuffer = []
+        global _parseddictionary
+        propertystartindex = 0
+        propertyendindex = 0
+        propertyvaluestartindex = 0
+        propertyvalueendindex = 0
+        propwasmarked = False
+        propvaluewasmarked = False
+        propertyname = ''
 
-	if data[0] != markercharacter or _hasreadxdf or len(markercharacter) > 1:
-		return False
-	else:
-		for character in data:
-			if isonproperty:
-				if propwasmarked == False:
-					propertystartindex = position
-					propwasmarked = True
-				else:
-					pass
+        if data[0] != markercharacter or _hasreadxdf or len(markercharacter) > 1:
+                return False
+        else:
+                for character in data:
+                        if isonproperty:
+                                if propwasmarked == False:
+                                        propertystartindex = position
+                                        propwasmarked = True
+                                else:
+                                        pass
 
-			if character == '=':
-					isonpropertyvalue = True
-					propertyendindex = position
-					propertyname = data[propertystartindex:propertyendindex].strip('\n')
-					propertyvaluestartindex = position + 1
+                        if character == '=':
+                                        isonpropertyvalue = True
+                                        propertyendindex = position
+                                        propertyname = data[propertystartindex:propertyendindex].strip('\n')
+                                        propertyvaluestartindex = position + 1
 
-			if isonpropertyvalue:
-				if character == markercharacter or position == len(data)-1:
-					isonmarker = True
-					propertyvalueendindex = position
-					_parseddictionary[propertyname] = data[propertyvaluestartindex:propertyvalueendindex].strip('\n')
-					propwasmarked = False
-				else:
-					pass
+                        if isonpropertyvalue:
+                                if character == markercharacter or position == len(data)-1:
+                                        isonmarker = True
+                                        propertyvalueendindex = position
+                                        _parseddictionary[propertyname] = data[propertyvaluestartindex:propertyvalueendindex].strip('\n')
+                                        propwasmarked = False
+                                else:
+                                        pass
 
-			if isonmarker:
-				isonproperty = True
-				isonmarker = False
-			position += 1
-		_hasreadxdf = True
-		return True
-		
+                        if isonmarker:
+                                isonproperty = True
+                                isonmarker = False
+                        position += 1
+                _hasreadxdf = True
+                return True
+                
 def reset():
         global debug
         _hasreadxdf = False
@@ -140,28 +138,28 @@ def getproperty(propertystring):
                 return False
 
 def setproperty(propertystring, propertyvalue):
-	_parseddictionary[propertystring] = propertyvalue
-	return True
+        _parseddictionary[propertystring] = propertyvalue
+        return True
 
 def writexdf(filepath, markercharacter, propertydictionary):
-		try:
-			file = open(filepath,'w')
-			for key in propertydictionary:
-				file.write(markercharacter+key+'='+str(propertydictionary[key])+"\n")
-			file.close()
-			return True
-		except PermissionError:
-			return False
+                try:
+                        file = open(filepath,'w')
+                        for key in propertydictionary:
+                                file.write(markercharacter+key+'='+str(propertydictionary[key])+"\n")
+                        file.close()
+                        return True
+                except PermissionError:
+                        return False
 
 def writexdf(filepath, propertydictionary):
-		try:
-			file = open(filepath,'w')
-			for key in propertydictionary:
-				file.write('@'+key+'='+str(propertydictionary[key])+"\n")
-			file.close()
-			return True
-		except PermissionError:
-			return False
+                try:
+                        file = open(filepath,'w')
+                        for key in propertydictionary:
+                                file.write('@'+key+'='+str(propertydictionary[key])+"\n")
+                        file.close()
+                        return True
+                except PermissionError:
+                        return False
 
 def _tellcurrentparserdictionary():
-	print(F"Listing {len(_parseddictionary)} properties and values in memory: \n{_parseddictionary}")
+        print(F"Listing {len(_parseddictionary)} properties and values in memory: \n{_parseddictionary}")
